25dcba972714981693e463a97508623f2e082753
schwarzam <gustavo.b.schwarz@gmail.com>
2024-01-04 09:48:15 -0300

    updated

--- cut_fits.py
+++ cut_fits.py
@@ -5,6 +5,7 @@ import io
 import argparse
 
 parser = argparse.ArgumentParser(description='Cut a FITS file')
+parser.add_argument('filepath', type=str, help='Path to the FITS file')
 parser.add_argument('ra', type=float, help='RA of the target in degrees')
 parser.add_argument('dec', type=float, help='Dec of the target in degrees')
 parser.add_argument('radius', type=float, help='Radius of the cutout in pixels')
@@ -14,9 +15,10 @@ args = parser.parse_args()
 ra = args.ra
 dec = args.dec
 radius = args.radius
+filepath = args.filepath
 
 # Run the C program and capture the output
-process = subprocess.run(["./main", ra, dec, radius], capture_output=True, check=True)
+process = subprocess.run(["./main", filepath, ra, dec, radius], capture_output=True, check=True)
 
 # Convert the output into a FITS file with Astropy
 hdu_list = fits.open(io.BytesIO(process.stdout))
